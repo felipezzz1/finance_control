93d2acf9c4035b9e183f381bd20059b26b72e841
felipezzz1 <felipezorzo78@gmail.com>
2025-11-15 16:31:27 -0300

    feat - add transaction entity and transaction repository

---
+++ src/main/java/com/fezor/finance_control/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.fezor.finance_control.repository;
+
+import com.fezor.finance_control.entity.Transaction;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class TransactionRepository implements PanacheRepository<Transaction> {
+
+}
