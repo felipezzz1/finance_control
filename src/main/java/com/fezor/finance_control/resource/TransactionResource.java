4b8603776273748d91edda67b000ce9c2ca84cca
felipezzz1 <felipezorzo78@gmail.com>
2025-11-17 23:02:10 -0300

    fix - added delete in transaction resource

--- src/main/java/com/fezor/finance_control/resource/TransactionResource.java
+++ src/main/java/com/fezor/finance_control/resource/TransactionResource.java
@@ -1,16 +1,12 @@
 package com.fezor.finance_control.resource;
 
 import com.fezor.finance_control.dto.TransactionCreateDTO;
-import com.fezor.finance_control.entity.Transaction;
-import com.fezor.finance_control.entity.User;
 import com.fezor.finance_control.service.TransactionService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.math.BigDecimal;
-import java.util.List;
 
 @Path("/transactions")
 @Produces(MediaType.APPLICATION_JSON)
