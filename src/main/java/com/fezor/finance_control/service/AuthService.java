1ee413d5502e8c37652931dd7207c6892a6e3dc8
felipezzz1 <felipezorzo78@gmail.com>
2025-11-18 23:22:33 -0300

    feat - auth service created

--- src/main/java/com/fezor/finance_control/service/AuthService.java
+++ src/main/java/com/fezor/finance_control/service/AuthService.java
@@ -1,4 +1,48 @@
 package com.fezor.finance_control.service;
 
+import com.fezor.finance_control.dto.LoginDTO;
+import com.fezor.finance_control.dto.LoginResponseDTO;
+import com.fezor.finance_control.dto.RegisterDTO;
+import com.fezor.finance_control.entity.User;
+import com.fezor.finance_control.repository.UserRepository;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.smallrye.jwt.build.Jwt;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.Set;
+
+@ApplicationScoped
 public class AuthService {
+
+    @Inject
+    UserRepository userRepository;
+
+    @Transactional
+    public void register(RegisterDTO dto){
+        User user = new User();
+        user.username = dto.username;
+        user.email = dto.email;
+
+        user.password = BcryptUtil.bcryptHash(dto.password);
+
+        userRepository.persist(user);
+    }
+
+    public LoginResponseDTO login(LoginDTO dto){
+        User user = userRepository.findByEmail(dto.email);
+
+        if (user == null || !BcryptUtil.matches(dto.password, user.password)) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        String token = Jwt.issuer("finance-control")
+                .subject(user.email)
+                .groups(Set.of("USER"))
+                .sign();
+
+        return new LoginResponseDTO(token, user.username);
+    }
+
 }
